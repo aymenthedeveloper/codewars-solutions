6faecbf8d4f42cad15a8353d59eadb88df461fe8
aymenthedeveloper <aymendev.contact@gmail.com>
2024-06-05 21:46:39 +0100

    solved increment string 5kyu kata

--- 5-kyu/incrementString.js
+++ 5-kyu/incrementString.js
@@ -1,14 +1,6 @@
-// try solve your kata here
-
 function incrementString(str) {
   let word = str.replace(/\d+\b/g, '');
   let [num] = str.match(/\d+\b/)||['0'];
   let n = +num + 1;
   return `${word}${'0'.repeat(Math.max(num.length-String(n).length, 0))}${n}`;
 }
-
-let data = 'f0f000';
-
-result = incrementString(data);
-
-console.log(result);
