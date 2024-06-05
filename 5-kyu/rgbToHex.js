78ea6a77e057c383781dcc735ef61113b8594727
aymenthedeveloper <aymendev.contact@gmail.com>
2024-06-05 11:24:19 +0100

    solved rgb to hex 5 kyu kata

--- 5-kyu/rgbToHex.js
+++ 5-kyu/rgbToHex.js
@@ -1,16 +1,6 @@
-// try solve your kata here
-
 function rgb(r, g, b) {
   return [r, g, b].map(v => {
       v = (v<0? 0: v>255? 255: v).toString(16)
       return v.length===1? 0+v: v;
     }).join('').toUpperCase();
 }
-
-let data = [0, 0, 0];
-data = [0,0,-20];
-data = [0,0,300]
-data = [173,255,47]
-result = rgb(...data);
-
-console.log(result);
